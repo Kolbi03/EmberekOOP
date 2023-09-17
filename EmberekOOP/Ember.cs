088ae868d64dcb0e54e09edf843e255a3cee42e4
Kolbi03 <lengyel.milan.hu@gmail.com>
2023-09-18 00:01:59 +0200

    Add project files.

---
+++ EmberekOOP/Ember.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmberekOOP
+{
+    internal class Ember
+    {
+        private string nev;
+        private string szulDatum;
+        private string szulHely;
+
+        public Ember(string nev, string szulDatum, string szulHely)
+        {
+            this.nev = nev;
+            this.szulDatum = szulDatum;
+            this.szulHely = szulHely;
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public string SzulDatum { get => szulDatum; set => szulDatum = value; }
+        public string SzulHely { get => szulHely; set => szulHely = value; }
+
+        public override string ToString()
+        {
+            return this.nev + " a nevem, ekkor születtem: " + this.szulDatum + " és ebben a városban: " + this.szulHely.ToString();
+        }
+
+        List<string> Datum = new List<string>();
+
+        public void Feltolt(int szam, string datum)
+        {
+            List<int> ev = new List<int>();
+            List<int> honap = new List<int>();
+            List<int> nap = new List<int>();
+
+
+            string[] datumok = new string[3];
+
+            for (int i = 0; i < szam; i++) 
+            {
+                datumok = datum.Split('.');
+
+                ev.Add(int.Parse(datumok[0]));
+                honap.Add(int.Parse(datumok[1]));
+                nap.Add(int.Parse(datumok[2]));
+            }
+
+            for (int i = 0;i < szam; i++)
+            {
+                Console.WriteLine(ev[i] + "/" + honap[i] + "/" + nap[i]);
+            }
+        }
+    }
+}
---
+++ EmberekOOP/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmberekOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ember ember1 = new Ember("Aladin", "2000.01.01", "Budapest");
+            Ember ember2 = new Ember("Leonidasz", "2001.01.01", "Budapest");
+            Ember ember3 = new Ember("Rajmund", "2002.01.01", "Budapest");
+
+            Console.WriteLine(ember1);
+            Console.WriteLine(ember2);
+            Console.WriteLine(ember3);
+
+            Ember.Feltolt();
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
